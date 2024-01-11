0d14afd693159c45747584d283afc453c4c0c4a3
Fakhir Iqbal <fakhiriqbal52@gmail.com>
2024-01-11 10:30:08 +0500

    Updated

--- function.js
+++ function.js
@@ -1,24 +1,45 @@
-const locationInput = document.getElementById('locationInput')
-        const getWeatherButton = document.getElementById('getWeather')
-        const weatherInfo = document.getElementById('weatherInfo')
-        let weatherData;
-        getWeatherButton.addEventListener('click', () => {
-        const location = locationInput.value
-        fetch(
-            `https://api.weatherapi.com/v1/current.json?key=91b4369798474fee84b51233233010&q=${location}&aqi=no`
-        )
+const weatherForm = document.getElementById('weatherForm');
+const locationInput = document.getElementById('locationInput');
+const weatherInfo = document.getElementById('weatherInfo');
+let weatherData;
+
+weatherForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevents the default form submission behavior
+    getWeather();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Invoke getWeather function with the default location (e.g., Karachi) when the page is loaded
+    getWeatherForDefaultLocation();
+});
+
+function getWeatherForDefaultLocation() {
+    const defaultLocation = 'Karachi';
+    locationInput.value = defaultLocation; // Set the input field to the default location
+    getWeather();
+}
+
+function getWeather() {
+    const location = locationInput.value.trim();
+
+    if (location === '') {
+        alert('Please Input Location!!!');
+        return;
+    }
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=91b4369798474fee84b51233233010&q=${location}&aqi=no`)
         .then(response => response.json())
         .then(data => {
-          let png = data.current.condition.icon ;
-          console.log(png)
-          console.log(data)
-          weatherData = data;
+            let png = data.current.condition.icon;
+            console.log(png);
+            console.log(data);
+            weatherData = data;
 
-          const weatherHtml = `<div class='weathercontainer'> 
+            const weatherHtml = `<div class='weathercontainer'> 
             <div class='row'> <h2 class="city-head" > Temperature in  ${data?.location?.name} </h2> 
                 <span> Temperature :</span> <span> ${data?.current?.temp_c}        <sup>o</sup> C </span> <br/>
                 <span> Feels Like  :</span> <span> ${data?.current?.feelslike_c}   <sup>o</sup> C </span> <br/>
-                <span> Humadity    :</span> <span> ${data?.current?.humidity} %    </span>  <br/>
+                <span> Humidity    :</span> <span> ${data?.current?.humidity} %    </span>  <br/>
                 <span> Wind        :</span> <span> ${data?.current?.wind_kph} km/h </span> 
             </div>
 
@@ -27,11 +48,11 @@ const locationInput = document.getElementById('locationInput')
                 <h4 class="city-head" > Temperature in  ${data?.current?.condition.text} </h4> 
             </div>
           
-          </div>`
+          </div>`;
 
-          weatherInfo.innerHTML = weatherHtml
+            weatherInfo.innerHTML = weatherHtml;
         })
         .catch(error => {
-          alert("Please Input Location!!!")
-        })
-        })
+            alert('Error fetching weather data. Please try again!');
+        });
+}
